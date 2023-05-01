eec3a1f6b0a1302c8d92d3192d6056de59ec8acd
Adarsh rawat <00adarshrawat00@gmail.com>
2023-05-01 13:33:45 +0530

    basic changes

--- data/database.js
+++ data/database.js
@@ -4,5 +4,5 @@ import mongoose from "mongoose";
 export const connectdb = ()=> mongoose.connect(process.env.MONGO_URI,{
     dbName:"API-Server",
 })
-.then((c)=>console.log(`database is connected`))
+.then((c)=>console.log(`database is connected with ${c.connection.host}`))
 .catch((e)=>{console.log(e)});
